c25b7572ccac984c3452d14c5f8a9f10edada4dc
Murat Memis <h.m.f.m.92@gmail.com>
2021-10-19 12:10:49 +0200

    initial commit
    Didnt have time to continue, will do it later today

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-calculator
+## Calculator
+
+- Ability to press buttons ranging from 0-9 and +/-/x/÷/=/,
+- Ability to delete numbers
+- Ability to plus and minus
+- Abiility to multiply and divice
---
+++ app.js
@@ -0,0 +1,68 @@
+//Buttons
+document.querySelector('#zero').onclick = () => {
+  console.log('0 Pressed')
+}
+
+document.querySelector('#one').onclick = () => {
+  console.log('1 Pressed')
+}
+
+document.querySelector('#two').onclick = () => {
+  console.log('2 Pressed')
+}
+
+document.querySelector('#three').onclick = () => {
+  console.log('3 Pressed')
+}
+
+document.querySelector('#four').onclick = () => {
+  console.log('4 Pressed')
+}
+
+document.querySelector('#five').onclick = () => {
+  console.log('5 Pressed')
+}
+
+document.querySelector('#six').onclick = () => {
+  console.log('6 Pressed')
+}
+
+document.querySelector('#seven').onclick = () => {
+  console.log('7 Pressed')
+}
+
+document.querySelector('#eight').onclick = () => {
+  console.log('8 Pressed')
+}
+
+document.querySelector('#nine').onclick = () => {
+  console.log('9 Pressed')
+}
+
+document.querySelector('#equals').onclick = () => {
+  console.log('= Pressed')
+}
+
+document.querySelector('#plus').onclick = () => {
+  console.log('+ Pressed')
+}
+
+document.querySelector('#minus').onclick = () => {
+  console.log('- Pressed')
+}
+
+document.querySelector('#multiply').onclick = () => {
+  console.log('x Pressed')
+}
+
+document.querySelector('#divide').onclick = () => {
+  console.log('÷ Pressed')
+}
+
+document.querySelector('#decimal').onclick = () => {
+  console.log(', Pressed')
+}
+
+document.querySelector('#clear').onclick = () => {
+  console.log('CLEAR Pressed')
+}

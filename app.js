9fe9aff2c1c2da67b8a911b01104a41e795c81f3
Murat Memis <h.m.f.m.92@gmail.com>
2021-10-26 19:09:40 +0200

    add Functionality
    added functionality to the operator and equals key.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const keys = document.querySelector('.calculator_keys')
 const display = document.querySelector('.calculator_display')
+const calculator = document.querySelector('.calculator')
 
 //Research addEventListerner more to learn about it
 keys.addEventListener('click', (e) => {
@@ -9,105 +10,48 @@ keys.addEventListener('click', (e) => {
     const keyContent = key.textContent
     const displayedNum = display.textContent
 
-    if (displayedNum === '0') {
-      display.textContent = keyContent
-    } else {
-      display.textContent = displayedNum + keyContent
-    }
-
     if (!action) {
+      display.textContent = keyContent
       console.log('number key')
     }
 
-    if (action === 'plus' || action === 'minus' || action === 'muitiply' || action === 'divide') {
+    if (action === 'plus' || action === 'minus' || action === 'multiply' || action === 'divide') {
+      calculator.dataset.firstValue = displayedNum
+      calculator.dataset.operator = action
       console.log('operator key')
     }
 
     if (action === 'decimal') {
+      display.textContent = displayedNum + ','
       console.log('decimal key')
     }
 
     if (action === 'clear') {
-      display.textContent = '0' //Need to actually find a logic to clear the display instead of setting it to 0
+      display.textContent = '' //Need to actually find a logic to clear the display instead of setting it to none
       console.log('clear key')
     }
 
     if (action === 'equals') {
+      let firstValue = calculator.dataset.firstValue
+      let operator = calculator.dataset.operator
+      let secondValue = displayedNum
+      display.textContent = equalize(firstValue, operator, secondValue)
       console.log('equal key')
     }
   }
 })
 
-//Buttons
-//Need to give logic to the specific buttons when pressed, cant really put my head around it for now
-//Redundant code?
+//To equalize the numbers you choose, but plus doesn't seem to work compared to the others?
+function equalize(numberOne, operator, numberTwo) {
+  if (operator === 'plus') {
+    result = numberOne + numberTwo
+  } else if (operator === 'minus') {
+    result = numberOne - numberTwo
+  } else if (operator === 'multiply') {
+    result = numberOne * numberTwo
+  } else if (operator === 'divide') {
+    result = numberOne / numberTwo
+  }
 
-// document.querySelector('#zero').onclick = () => {
-//   console.log('0 Pressed')
-//   document.querySelector('#display').innerHTML = 'Test'
-// }
-
-// document.querySelector('#one').onclick = () => {
-//   console.log('1 Pressed')
-// }
-
-// document.querySelector('#two').onclick = () => {
-//   console.log('2 Pressed')
-// }
-
-// document.querySelector('#three').onclick = () => {
-//   console.log('3 Pressed')
-// }
-
-// document.querySelector('#four').onclick = () => {
-//   console.log('4 Pressed')
-// }
-
-// document.querySelector('#five').onclick = () => {
-//   console.log('5 Pressed')
-// }
-
-// document.querySelector('#six').onclick = () => {
-//   console.log('6 Pressed')
-// }
-
-// document.querySelector('#seven').onclick = () => {
-//   console.log('7 Pressed')
-// }
-
-// document.querySelector('#eight').onclick = () => {
-//   console.log('8 Pressed')
-// }
-
-// document.querySelector('#nine').onclick = () => {
-//   console.log('9 Pressed')
-// }
-
-// document.querySelector('#equals').onclick = () => {
-//   console.log('= Pressed')
-// }
-
-// document.querySelector('#plus').onclick = () => {
-//   console.log('+ Pressed')
-// }
-
-// document.querySelector('#minus').onclick = () => {
-//   console.log('- Pressed')
-// }
-
-// document.querySelector('#multiply').onclick = () => {
-//   console.log('x Pressed')
-// }
-
-// document.querySelector('#divide').onclick = () => {
-//   console.log('÷ Pressed')
-// }
-
-// document.querySelector('#decimal').onclick = () => {
-//   console.log(', Pressed')
-// }
-
-// document.querySelector('#clear').onclick = () => {
-//   console.log('CLEAR Pressed')
-//   document.querySelector('#display').innerHTML = 'Clear test'
-// }
+  return result
+}
